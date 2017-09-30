58371aa64a19bdd422bbf427147bafdb4684d52b
ardiansyah97 <ardiansyah_siskom14@mahasiswa.unj.ac.id>
2017-09-30 12:27:39 +0700

    GOIndoensia

---
+++ desktop/src/com/mobcom/goindonesia/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mobcom.goindonesia.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mobcom.goindonesia.GOIndonesia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GOIndonesia(), config);
+	}
+}
